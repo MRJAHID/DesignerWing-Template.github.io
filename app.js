2293226eb22c7819d9194353ec3c023b3b98278b
MRJAHID <mrjahid350@gmail.com>
2022-12-15 16:39:03 +0600

    Upload

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 console.clear();
-import ImageCompare from "image-compare-viewer";
 
 document.addEventListener("DOMContentLoaded", function () {
     var before_after = document.querySelectorAll('.before_after');
@@ -44,10 +43,79 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// Cliping path image
-// const clipImageContainer = document.querySelector('.clipimage_container');
-// const clipImageSlider = document.querySelector('.clipimg_slider');
-// clipImageSlider.addEventListener('input', (e) => {
-//     clipImageContainer.style.setProperty('--position', `${e.target.value}%`);
-// })
-// Call & init
+// Upload image
+window.addEventListener("DOMContentLoaded", () => {
+    const dragDropAreas = document.getElementsByClassName("drag-drop-area");
+
+    for (let area of dragDropAreas) {
+        let parentEl = area.parentElement.parentElement;
+        let progressThumb = parentEl.querySelector(
+            ".drag-drop-progress-loader-thumb"
+        );
+        let uploadBtn = parentEl.querySelector(".drag-drop-area-button");
+        let fileDialog = parentEl.querySelector(".file-dialog");
+
+        const onUploadFiles = (files) => {
+            console.log(files) // FileList → https://developer.mozilla.org/en-US/docs/Web/API/FileList
+
+            progressThumb.style.width = 0;
+
+            if (parentEl.classList.contains("active")) {
+                parentEl.classList.remove("active");
+            }
+
+            parentEl.classList.add("dropped-anim");
+            parentEl.classList.add("dropped");
+
+            setTimeout(() => {
+                parentEl.classList.remove("dropped-anim");
+
+                // Fake upload simulation
+                let uploadProgress = 0;
+                let timer = setInterval(() => {
+                    uploadProgress++;
+
+                    progressThumb.style.width = uploadProgress + "%";
+
+                    if (uploadProgress === 100) {
+                        clearInterval(timer);
+                        setTimeout(() => {
+                            parentEl.classList.remove("dropped"); // Remove this class when uploading is finished
+                        }, 400);
+                    }
+                }, 15);
+            }, 800);
+        }
+
+        // Upload using dragging
+        area.addEventListener("dragenter", (e) => {
+            e.preventDefault();
+            if (!parentEl.classList.contains("active")) {
+                parentEl.classList.add("active");
+            }
+        });
+        area.addEventListener("dragleave", (e) => {
+            e.preventDefault();
+            if (parentEl.classList.contains("active")) {
+                parentEl.classList.remove("active");
+            }
+        });
+        area.addEventListener("drop", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            onUploadFiles(e.dataTransfer.files);
+        });
+        area.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
+
+        // Upload using button
+        uploadBtn.addEventListener("click", () => {
+            fileDialog.click();
+        });
+        fileDialog.addEventListener("change", () => {
+            onUploadFiles(fileDialog.files);
+        });
+    }
+});
